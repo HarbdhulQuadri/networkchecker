4ee0a5ecb82c22b6794175b80b43013a5c8a0af6
Abdulquadri Adegbiji <33990653+HarbdhulQuadri@users.noreply.github.com>
2020-01-18 22:05:48 +0100

    program to check for network provider

---
+++ network.py
@@ -0,0 +1,24 @@
+﻿#DEFINING THE PROVIDERS  
+mtn = [703,706,803,806,810,813,814,816,903,906]
+glo =[705,805,807,811,815,905]
+airtel=[701,708,802,808,812,902,907,901]
+etisalat = [809,817,818,908,909]
+
+#Accepting Input
+number = input('ínput your number')
+
+#selecting the first 3 digit to determine the network provider
+provider = int(number[1:4])
+
+#checking the  provider
+if provider in mtn:
+    print("MTN  number")
+elif provider in glo:
+    print("GLO number")
+elif provider in airtel:
+    print("Airtel number")
+elif provider in etisalat:
+    print("(9mobile )")
+else:
+    print('number not found')
+            
